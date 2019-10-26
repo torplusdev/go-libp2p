592abf8011869478ea87df982007b658f27cff4d
Michael Parshin <parshim@gmail.com>
2019-10-26 22:24:20 +0300

    Removed default Tor

--- defaults.go
+++ defaults.go
@@ -11,7 +11,6 @@ import (
 	secio "github.com/libp2p/go-libp2p-secio"
 	yamux "github.com/libp2p/go-libp2p-yamux"
 	tcp "github.com/libp2p/go-tcp-transport"
-	tor "paidpiper.com/go-libp2p-onion-transport"
 	ws "github.com/libp2p/go-ws-transport"
 	multiaddr "github.com/multiformats/go-multiaddr"
 )
@@ -37,7 +36,7 @@ var DefaultMuxers = ChainOptions(
 // libp2p instead of replacing them.
 var DefaultTransports = ChainOptions(
 	Transport(tcp.NewTCPTransport),
-	Transport(tor.NewOnionTransport),
+	//Transport(tor.NewOnionTransport),
 	Transport(ws.New),
 )
 
